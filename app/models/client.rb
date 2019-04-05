b60a97a4894d82405a8a0972e2d89b245df2552e
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-04-05 09:06:58 -0500

    Update client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,5 @@
 class Client < ApplicationRecord
+  has_many :pets
   validates_each :birth_date do |record, attr, value|
     record.errors.add attr, "does not meet Terms of Service requirements(over 18)" if value > (18.years.ago).to_date
   end
